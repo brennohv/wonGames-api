83e6209763da41180764dcb9ea33328cdbbe8799
Brenno Henrique <brennovicentini@gmail.com>
2022-01-19 11:01:12 +0000

    Add nodemailer and others

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 10000,
+    apolloServer: {
+      tracing: true,
+    },
+  },
+};
