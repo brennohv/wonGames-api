01c32168a75f1039065bb51689252c4b81110b96
Brenno Henrique <brennovicentini@gmail.com>
2022-02-02 16:30:45 +0000

    Install Stripe and add env key

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,9 +1,7 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
 
 module.exports = {
   createPaymentIntent: async (ctx) => {
@@ -28,12 +26,17 @@ module.exports = {
       return {
         error: 'No valid games found'
       }
-    }
+    };
 
     const total = games.reduce((acc, game) => {
       return acc + game.price
     }, 0);
 
+    if(total === 0) {
+      return {
+        freeGames: true
+      }
+    }
     return {total_in_cents: total * 100 ,games}
   },
 };
